89aff869aa95b1da18320412279a04d77a9e8e5d
KAIMonmoy <aftarulislam@gmail.com>
2021-07-24 09:35:56 +0600

    :sparkles: Added Meetings

--- core/urls.py
+++ core/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     path("<int:pk>/team/", TeamMemberListCreate.as_view(), name="teammember-list-create"),
     path("<int:pk>/updates/", UpdateList.as_view(), name="update-list"),
     path("<int:workspace_id>/team/<int:pk>/", TeamMemberRetrieveUpdateDelete.as_view(), name="teammember-detail"),
+    path("<int:pk>/meetings/", MeetingListCreate.as_view(), name="meeting-list-create"),
 ]
+
--- core/views.py
+++ core/views.py
@@ -158,7 +158,6 @@ class TeamMemberRetrieveUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
             return Response({'error': 'Access restricted to workspace members only'}, status=status.HTTP_403_FORBIDDEN)
         team_member = get_object_or_404(TeamMember, pk=pk)
         serializer = TeamMemberSerializer(team_member)
-        print(serializer.data)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def update(self, request, *args, **kwargs):
@@ -208,3 +207,95 @@ class UpdateList(generics.ListAPIView):
         serializer = UpdateSerializer(update_queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+
+class MeetingListCreate(generics.ListCreateAPIView):
+    serializer_class = MeetingSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Meeting.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        workspace = get_object_or_404(Workspace, pk=pk)
+        request.data['workspace'] = pk
+
+        if not TeamMember.objects.filter(member=request.user, workspace=workspace, role='admin').exists():
+            return Response({'error': 'Access restricted to workspace admins only'}, status=status.HTTP_403_FORBIDDEN)
+
+        serializer = MeetingSerializer(data=request.data)
+        if serializer.is_valid():
+            instance = serializer.save()
+        else:
+            return Response({'error': 'Invalid arguments for creating meetings'}, status=status.HTTP_400_BAD_REQUEST)
+
+        update = Update()
+        update.workspace = workspace
+        update.message = f'Meeting - {instance.agenda} is schedueld for {str(instance.start_time).split("+")[0]} \
+by {request.user.user_name}'
+        update.save()
+
+        participants = request.data.get('participants', None)
+
+        if participants is not None:
+            email_list = []
+            for user_id in participants:
+                try:
+                    meeting_user = PrismUser.objects.get(pk=user_id)
+                    if TeamMember.objects.filter(member=meeting_user, workspace=workspace).exists():
+                        meetint_participant = MeetingParticipant()
+                        meetint_participant.participant = meeting_user
+                        meetint_participant.meeting = instance
+                        meetint_participant.save()
+                        email_list.append(meeting_user.email)
+                except PrismUser.DoesNotExist:
+                    print(f'{user_id} does not exist')
+
+            send_email(
+                recipients=email_list,
+                subject=f"Meeting - {instance.agenda}",
+                message="""
+                <table style="min-width: 480px;">
+                <thead>
+                    <th colspan="2" style="font-size: 24px; padding: 20px;">Prism - Meeting</th>
+                </thead>
+                <tr>
+                    <td style="border: 1px solid #ccc; font-size: 18px; padding: 8px 20px;">Agenda</td>
+                    <td style="border: 1px solid #ccc; font-size: 18px; padding: 8px 20px;">"""
+                        + instance.agenda + """</td>
+                </tr>
+                <tr>
+                    <td style="border: 1px solid #ccc; font-size: 18px; padding: 8px 20px;">Time</td>
+                    <td style="border: 1px solid #ccc; font-size: 18px; padding: 8px 20px;">"""
+                        + str(instance.start_time) + """</td>
+                </tr>
+                <tr>
+                    <td style="border: 1px solid #ccc; font-size: 18px; padding: 8px 20px;">Duration</td>
+                    <td style="border: 1px solid #ccc; font-size: 18px; padding: 8px 20px;">"""
+                        + str(instance.duration_mins) + """ Minutes</td>
+                </tr>
+                <tr>
+                    <td style="border: 1px solid #ccc; font-size: 18px; padding: 8px 20px;">Link</td>
+                    <td style="border: 1px solid #ccc; font-size: 18px; padding: 8px 20px;"><a href="""
+                        + '"' + instance.link + '"'
+                        + """>""" + instance.link + """</a></td>
+                </tr>
+                <tr>
+                    <td style="border: 1px solid #ccc; font-size: 18px; padding: 8px 20px;">Inviter</td>
+                    <td style="border: 1px solid #ccc; font-size: 18px; padding: 8px 20px;">"""
+                        + request.user.user_name + """</td>
+                </tr>
+                </table>"""
+            )
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def list(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        user = request.user
+        workspace = get_object_or_404(Workspace, pk=pk)
+        if not TeamMember.objects.filter(member=user, workspace=workspace).exists():
+            return Response({'error': 'Access restricted to workspace members only'}, status=status.HTTP_403_FORBIDDEN)
+        meeting_queryset = Meeting.objects.filter(workspace=workspace)
+        serializer = MeetingSerializer(meeting_queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
